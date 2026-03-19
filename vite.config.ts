9e61e294fc35fe4e1140d4cd09039a2401a3a81a
Rafael Miranda <rafacmarn@gmail.com>
2026-03-19 12:08:45 -0300

    fix: nome do app

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/d&j-radiadores/",
+  base: "/dej-radiadores/",
   plugins: [vue()],
   server: {
     host: true,
