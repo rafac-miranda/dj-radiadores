9e53645a61da8cddead8fa9e02216d21c3af66fc
Rafael Miranda <rafacmarn@gmail.com>
2026-03-19 12:19:42 -0300

    fix: nome do app

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dej-radiadores/",
+  base: "/",
   plugins: [vue()],
   server: {
     host: true,
     port: 5173,
     strictPort: true,
     hmr: {
-      host: 'localhost',
       port: 5173
     }
   }
